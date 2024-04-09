73a99a57f6a0bc44186fb452e3efeed233e4676d
Kate <chenk18@outlook.com>
2024-04-08 22:12:34 -0700

    finish the final version and readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Finance Data Explorer
+
+Kate Chen
+
+## Getting Started
+
+```bash
+# This time we will not use virtual env  
+pip install -r requirements.txt
+
+streamlit run app.py
+```
+
+## Deployment Page
+
+https://techin510lab2-sdbz2ux7y5wuqvjzpk6btx.streamlit.app/
+
+## What I learned
+
+- Use text element to write title and text of web page
+- Use slider to filter a certain range of data
+- Use bar chart to visulize the data
+
+## Questions
+
+- How can I show the relationship between two or more columns of data? 
--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 st.set_page_config(
     page_title = "Titanic data",
@@ -10,6 +13,9 @@ st.set_page_config(
 )
 
 st.title("Titanic people data")
+
+st.text("This dataset is the passenger information of Taitanic. The web page has a filter to age range, and visulizes the age distribution data.")
+
 
 low, high = st.slider(
     'Select a age range of people',
@@ -22,3 +28,22 @@ titanic = pd.read_csv(url)
 
 st.dataframe(titanic[(titanic['age'] >= low) & (titanic['age'] <= high)])
 
+df = titanic.copy()
+
+# create bins
+
+df['bin'] = pd.cut(df['age'], bins=list(range(0, 86, 5)))
+
+# summarize the data
+summary = df['bin'].value_counts().sort_index()
+
+# draw plot
+plt.figure(figsize=(10, 6))
+plt.bar(summary.index.astype(str), summary.values, color = "pink")
+plt.xticks(rotation=45)
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.title('Age distribution')
+
+# 在Streamlit中显示图形
+st.pyplot(plt)
